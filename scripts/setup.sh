ccced903caf0cbf3038b5836e3be168605a919d3
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-02-22 09:30:49 -0600

    fix script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,6 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+source /exp/dune/app/users/awilkins/tdr_dunetpc_larsoft/localProducts_larsoft_v07_06_02_e17_prof/setup
 unsetup mrb
 setup mrb v4_04_06
-setup cmake v3_12_1
-source /exp/dune/app/users/awilkins/tdr_dunetpc_larsoft/localProducts_larsoft_v07_06_02_e17_prof/setup
 mrbslp
 
