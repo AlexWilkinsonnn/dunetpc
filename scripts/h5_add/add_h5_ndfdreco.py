4eaa85d5d0859eccdb83e4c032c322cd83e688e1
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-03-27 10:17:26 -0500

    update h5 add script for multiple dirs

--- scripts/h5_add/add_h5_ndfdreco.py
+++ scripts/h5_add/add_h5_ndfdreco.py
@@ -7,11 +7,16 @@ import h5py
 import numpy as np
 
 def main(args):
-    ret = subprocess.run(["ifdh", "ls", args.input_dir], stdout=subprocess.PIPE)
-    f_paths = ret.stdout.decode("utf-8").strip("\n").split("\n")[1:] # first is directory
-
     f_out = h5py.File(args.output_file, "w")
+    
+    for input_dir in args.input_dir.split(","):
+        ret = subprocess.run(["ifdh", "ls", input_dir], stdout=subprocess.PIPE)
+        f_paths = ret.stdout.decode("utf-8").strip("\n").split("\n")[1:] # first is directory
+        fill_h5(f_paths, f_out)
 
+    f_out.close()
+
+def fill_h5(f_paths, f_out):
     for f_path in f_paths:
         f_name = os.path.basename(f_path)
 
@@ -23,23 +28,23 @@ def main(args):
                 nd_reco = f_in["nd_paramreco"]
                 fd_reco = f_in["fd_reco"]
                 
-                f_out.create_group(f_name)
-                f_out[f_name].create_dataset("nd_paramreco", data=nd_reco)
-                f_out[f_name].create_dataset("fd_reco", data=fd_reco)
+                f_out.create_group(f_path)
+                f_out[f_path].create_dataset("nd_paramreco", data=nd_reco)
+                f_out[f_path].create_dataset("fd_reco", data=fd_reco)
     
-                print(f"Added {f_name}")
+                print(f"Added {f_path}")
             else:
-                print(f"No FD reco for {f_name}")
+                print(f"No FD reco for {f_path}")
 
         proc = subprocess.Popen(["rm", "-v", f_name], stdout=subprocess.PIPE)
         proc.wait()
 
-    f_out.close()
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("input_dir")
+    parser.add_argument(
+        "input_dir", type=str, help="Can be comma separated if files in multiple dirs"
+    )
     parser.add_argument("output_file")
 
     args = parser.parse_args()
