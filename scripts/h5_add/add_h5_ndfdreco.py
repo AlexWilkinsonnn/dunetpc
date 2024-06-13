87e4fff6966b59d5086c459c2717e27cc88b9b19
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-06-13 14:38:16 -0500

    catch and skip corrupted h5 files in the h5 add script

--- scripts/h5_add/add_h5_ndfdreco.py
+++ scripts/h5_add/add_h5_ndfdreco.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def main(args):
     f_out = h5py.File(args.output_file, "w")
-    
+
     for input_dir in args.input_dir.split(","):
         ret = subprocess.run(["ifdh", "ls", input_dir], stdout=subprocess.PIPE)
         f_paths = ret.stdout.decode("utf-8").strip("\n").split("\n")[1:] # first is directory
@@ -25,20 +25,26 @@ def fill_h5(f_paths, f_out):
 
         group_name = f_path.replace("/", "#")
 
-        with h5py.File(f_name) as f_in:
-            if "fd_reco" in f_in.keys() and "nd_paramreco" in f_in.keys():
-                nd_reco = f_in["nd_paramreco"]
-                fd_reco = f_in["fd_reco"]
-                fd_vertices = f_in["fd_vertices"]
-                
-                f_out.create_group(group_name)
-                f_out[group_name].create_dataset("nd_paramreco", data=nd_reco)
-                f_out[group_name].create_dataset("fd_reco", data=fd_reco)
-                f_out[group_name].create_dataset("fd_vertices", data=fd_vertices)
-    
-                print(f"Added {group_name}")
-            else:
-                print(f"Missing fd_reco or nd_paramreco for {f_path}:\n({f_in.keys()})")
+        try:
+            f_in = h5py.File(f_name)
+        except Exception as e:
+            print(f"Failed to read h5py file {f_name} with exception:")
+            print(e)
+        else:
+            with h5py.File(f_name) as f_in:
+                if "fd_reco" in f_in.keys() and "nd_paramreco" in f_in.keys():
+                    nd_reco = f_in["nd_paramreco"]
+                    fd_reco = f_in["fd_reco"]
+                    fd_vertices = f_in["fd_vertices"]
+
+                    f_out.create_group(group_name)
+                    f_out[group_name].create_dataset("nd_paramreco", data=nd_reco)
+                    f_out[group_name].create_dataset("fd_reco", data=fd_reco)
+                    f_out[group_name].create_dataset("fd_vertices", data=fd_vertices)
+
+                    print(f"Added {group_name}")
+                else:
+                    print(f"Missing fd_reco or nd_paramreco for {f_path}:\n({f_in.keys()})")
 
         proc = subprocess.Popen(["rm", "-v", f_name], stdout=subprocess.PIPE)
         proc.wait()
